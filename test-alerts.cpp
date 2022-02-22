835fcb9f506f68314bc32b5c21ef5918bc952af4
SujnaR-Bhat <91068079+SujnaR-Bhat@users.noreply.github.com>
2022-02-22 22:43:33 +0530

    Update test-alerts.cpp

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -6,20 +6,25 @@
 TEST_CASE("infers the breach according to limits")
 {
   REQUIRE(inferBreach(12, 20, 30) == TOO_LOW);
-  REQUIRE(inferBreach(60, 20, 30) == TOO_HIGH);
-  REQUIRE(inferBreach(27, 20, 30) == NORMAL);
+  REQUIRE(inferBreach(60, 35, 45) == TOO_HIGH);
+  REQUIRE(inferBreach(27, 45, 65) == NORMAL);
 }
 
 TEST_CASE("infers the breach according to cooling type") 
 {
   REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING, -1) == TOO_LOW);
-  REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING, 20) == NORMAL);
-  REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING, 36) == TOO_HIGH);
   REQUIRE(classifyTemperatureBreach(HI_ACTIVE_COOLING, -2) == TOO_LOW);
-  REQUIRE(classifyTemperatureBreach(HI_ACTIVE_COOLING,20) == NORMAL);
-  REQUIRE(classifyTemperatureBreach(HI_ACTIVE_COOLING, 46) == TOO_HIGH); 
   REQUIRE(classifyTemperatureBreach(MED_ACTIVE_COOLING, -3) == TOO_LOW);
+  
+  REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING, 20) == NORMAL);
+  REQUIRE(classifyTemperatureBreach(HI_ACTIVE_COOLING,20) == NORMAL);
   REQUIRE(classifyTemperatureBreach(MED_ACTIVE_COOLING, 20) == NORMAL);
+  
+  REQUIRE(classifyTemperatureBreach(HI_ACTIVE_COOLING, 46) == TOO_HIGH); 
+  REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING, 36) == TOO_HIGH);
   REQUIRE(classifyTemperatureBreach(MED_ACTIVE_COOLING, 41) == TOO_HIGH);
 }
+
+
+
 
